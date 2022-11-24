bba33d8e587eab106f5e442f4a8421f349f1fa27
rwtam <rwtam@caltech.edu>
2022-11-23 16:34:05 -0800

    Break unit test

--- app.py
+++ app.py
@@ -1,15 +1,32 @@
-"""
-Main application file:
-a simple Flask web application.  This application contains one endpoint that reverses and returns the requested URI
-"""
+# """
+# Main application file:
+# a simple Flask web application.  This application contains one endpoint that reverses and returns the requested URI
+# """
 
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/<random_string>')
+# def returnBackwardsString(random_string):
+#     """Reverse and return the provided URI"""
+#     return "".join(reversed(random_string))
+
+# if __name__ == '__main__':
+#     app.run(host='0.0.0.0', port=8080)
+
+
+
+"""
+Main application file
+This is to break the unit test
+"""
 from flask import Flask
 app = Flask(__name__)
 
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
-    return "".join(reversed(random_string))
+    return "Breaking the unit test"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
