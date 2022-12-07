611ceb6a6aa0fe0474a351d063d5b9ce19424940
rwtam <rwtam@caltech.edu>
2022-12-07 13:38:14 -0800

    Quakes2AWS mod

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,30 @@
 #instructions for building the application container image
+#PREVIOUS ONES USED
+# FROM python:3
+# # Set application working directory
+# WORKDIR /usr/src/app
+# # Install requirements
+# COPY requirements.txt ./
+# RUN pip install --no-cache-dir -r requirements.txt
+# # Install application
+# COPY app.py ./
+# # Run application
+# CMD python app.py
+
+
+#USED NOW FOR THE QUAKES2AWS DB interface 
 FROM python:3
 # Set application working directory
 WORKDIR /usr/src/app
+COPY common-layer/dependencies /tmp/dependencies 
+
 # Install requirements
 COPY requirements.txt ./
 RUN pip install --no-cache-dir -r requirements.txt
+
+RUN find . -name "*.pyc" -delete 
+#to avoid bad magic number errors? https://stackoverflow.com/questions/514371/whats-the-bad-magic-number-error
+
 # Install application
 COPY app.py ./
 # Run application
--- app_older.py
+++ app_older.py
@@ -38,12 +38,7 @@ added secret keys
 """
 from flask import Flask
 import logging
-import time
 app = Flask(__name__)
-
-from quakes2aws_datastore.logging import logger  # noqa:E402
-from django.db import connection
-from contexttimer import Timer
 
 # Initialize Logger
 LOGGER = logging.getLogger()
@@ -56,23 +51,5 @@ def returnBackwardsString(random_string):
     return "".join(reversed(random_string))
 
 if __name__ == '__main__':
-    #Can set a while True, time.sleep later on to simulate, want to make sure it has access to our DB
-    while True:
-        with Timer() as fetch_time:
-            with connection.cursor() as cursor:
-                cursor.execute('SELECT endtime FROM sample_set ORDER BY "id" DESC LIMIT 1;') 
-                row = cursor.fetchall() 
-                latest_endtime=row[0][0] #should be of type 'float'
-        fetch_time_items_elapsed=round(fetch_time.elapsed,3)
-
-        logger.info(
-            'information',
-            fetch_time=fetch_time_items_elapsed,
-            latest_endtime_found=latest_endtime)
-        
-        time_sleeping=30-fetch_time_items_elapsed
-        
-        time.sleep(time_sleeping) #30-the time it took to fetch
-
-
-    # app.run(host='0.0.0.0', port=8080)
+    print("Ryan testing the endpoint")
+    app.run(host='0.0.0.0', port=8080)
